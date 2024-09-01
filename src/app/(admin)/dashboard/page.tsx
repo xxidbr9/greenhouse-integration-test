d952eb9918c2b52a2c31be230488fe0461f9949f
Barnando Akbarto Hidayatullah <barnando13@gmail.com>
2024-09-02 01:46:40 +0700

    fix: change file internal

--- src/app/(admin)/dashboard/page.tsx
+++ src/app/(admin)/dashboard/page.tsx
@@ -3,22 +3,12 @@ import { DataTable } from "./data-table";
 import { columns } from "./columns";
 import { Toaster } from "sonner";
 
-async function getData({
-  page,
-  perPage,
-}: {
-  page: number;
-  perPage: number;
-}): Promise<CandidateType[]> {
+export default async function DashboardPage() {
   const resp = await fetch(
     `${process.env.NEXT_PUBLIC_LOCAL_URL}/api/candidates?page=`,
   );
   const json = await resp.json();
-  return (await json.data) as CandidateType[];
-}
-
-export default async function DemoPage() {
-  const data = await getData({ page: 1, perPage: 10 });
+  const data = (await json.data) as CandidateType[];
 
   return (
     <div className="container mx-auto py-10">
